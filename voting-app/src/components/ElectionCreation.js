8f726e68117b6e4d1ecf15752be00544c0e8d2bf
gsargsyan <grigor_sargsyan@intuit.com>
2021-09-30 18:51:24 -0700

    Grigor: clean up

--- voting-app/src/components/ElectionCreation.js
+++ voting-app/src/components/ElectionCreation.js
@@ -2,12 +2,7 @@
 import { ElectionCreationFormConatiner } from '../containers/ElectionCreationFormContainer'
 import { ElectionCreationTableContainer} from '../containers/ElectionCreationTableContainer'
 
-
-
 export const ElectionCreation = () => {
-    //Create Form
-    //Create Tables
-    //create view row
     return (
         <>
             <h1>ElectionCreation</h1>
